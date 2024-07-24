215b474ef353a73d27dfa6d8f1a2f180ec800ff2
Wasim <jshakir005@gmail.com>
2024-07-25 00:25:44 +0530

    going to learn expo components so this repo closed for now, and starting with new repo -> learn_expo

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from 'expo-router';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>fuck you</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">fuck you</Text>
             <StatusBar style="auto" />
-            <Link href="/profile">goto profile</Link>
+            <Link href="/home">goto profile</Link>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
